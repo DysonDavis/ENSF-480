b6af07c120f49a9b3441387f64e417dcfc8dbf70
Dyson Davis <dysondavis0@gmail.com>
2022-10-12 21:56:08 -0600

    Edit

--- rectangle.cpp
+++ rectangle.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <filesystem>
 using namespace std;
 
 Rectangle::Rectangle(double a, double b, double side1, double side2, string name): Square(a, b, side1, name){
